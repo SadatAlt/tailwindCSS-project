6798338be6f0ef2c9127e0252ab0d9dc24ff1da5
SadatAlt <arman142752@gmail.com>
2025-01-13 00:37:01 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ["*"],
+    theme: {
+      extend: {
+            colors:{
+              headerBg: "#160C6D",
+              hoverColor: "#1BBF00",
+              overlay:"rgba(106, 98, 197, 0.9)",
+            },
+            fontFamily:{
+              first: ['Open Sans'],
+              second:['Paprika'],
+            },
+            maxWidth:{
+            header:"1170px",
+            },
+            backgroundImage:{
+              bannerBg:"url('../images/banner.png')"
+            }
+      },
+    },
+    plugins: [],
+  }
